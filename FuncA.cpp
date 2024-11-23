a6eda6f6c0bea4f93d04361a3d6c79d434da0784
Trembach Anna <trembach.ann@gmail.com>
2024-11-23 16:20:35 +0200

    hotfix: sum only the first three terms

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,6 +1,11 @@
 #include "FuncA.h"
 
 double FuncA::calculate() {
-    return 0;  
+    double sum = 0;
+    for (int i = 0; i < 3; i++) {
+        double term = pow(-1, i) * (pow(2, i) * factorial(2 * i)) / (pow(4, i) * pow(factorial(i), 2) * (2 * i + 1));
+        sum += term;
+    }
+    return sum;
 }
 
