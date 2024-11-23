6e8c8dd3a68fe43e979cddda225c5d83d0b13502
Trembach Anna <trembach.ann@gmail.com>
2024-11-23 21:49:08 +0200

    add files

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,6 +1,22 @@
-#include "FuncA.h"
+#include "FuncA.h"  
+#include <cmath>    
+
+int factorial(int n) {
+    if (n <= 1) return 1;
+    int result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
 
 double FuncA::calculate() {
-    return 0;  
+    double sum = 0;
+    for (int i = 0; i < 3; i++) {
+        double term = pow(-1, i) * (pow(2, i) * factorial(2 * i)) /
+                      (pow(4, i) * pow(factorial(i), 2) * (2 * i + 1));
+        sum += term;
+    }
+    return sum;
 }
 
