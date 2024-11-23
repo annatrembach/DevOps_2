f209c02bd41215e3fd88a80c0a6c829a8a55b86a
Trembach Anna <trembach.ann@gmail.com>
2024-11-23 16:23:29 +0200

    finalize FuncA implementation with parameter n

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,6 +1,22 @@
 #include "FuncA.h"
+#include <cmath> 
 
-double FuncA::calculate() {
-    return 0;  
+double FuncA::calculate(int n) {
+    // This function calculates the sum of the first n terms of the infinite series
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        double term = pow(-1, i) * (pow(2, i) * factorial(2 * i)) / (pow(4, i) * pow(factorial(i), 2) * (2 * i + 1));
+        sum += term;
+    }
+    return sum;
+}
+
+double FuncA::factorial(int num) {
+    if (num == 0 || num == 1) return 1;
+    double fact = 1;
+    for (int i = 2; i <= num; i++) {
+        fact *= i;
+    }
+    return fact;
 }
 
